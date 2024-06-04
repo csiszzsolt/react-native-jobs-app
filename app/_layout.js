cb0b02c2509c43e148d5bf8bef5280c6e66bd345
Zsolt Csiszér <88651751+Csiszer@users.noreply.github.com>
2024-06-04 16:34:38 +0300

    Updated home and details screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,18 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
+
+// SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+  });
+
+  if(!fontsLoaded) return null;
+
+  return <Stack/>;
 }
